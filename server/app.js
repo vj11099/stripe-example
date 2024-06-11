ae7ba4dbddd7ee1944365b9db8f23f316e1a9d91
Vishvam Joshi <vishvamjoshi11099@gmail.com>
2024-06-11 18:37:40 +0530

    CHORE: initiated express server and a vite react project

---
+++ server/app.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express, { json } from "express";
+import { connect } from "mongoose";
+
+const app = express();
+
+app.use(json());
+
+const URI = process.env.MONGO_URI;
+
+//database connection
+connect(URI)
+  .then((res) => {
+    console.log("Listening to Port 3001");
+    app.listen(3001);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
